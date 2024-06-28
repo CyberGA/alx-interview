153d52c7ded0782580d64327ce09e193b063df73
CyberGA <etomu.joshua@gmail.com>
2024-06-27 21:10:29 -0700

    update: complete tasks

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,16 @@
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+    :param grid: List of lists representing the grid.
+    :return: Integer representing the perimeter of the island.
+    """
+    perimeter = 0
+
+    # Traverse rows and columns
+    for row in grid + list(map(list, zip(*grid))):
+        # Compare each cell with its next neighbor (including boundary with 0)
+        for i1, i2 in zip([0] + row, row + [0]):
+            if i1 != i2:
+                perimeter += 1
+
+    return perimeter
